5f39d72dcfed8e8677ec582d1a10d118feaea47e
Hugo Torrico <torricomarquezh@gmail.com>
2025-02-12 22:23:23 -0500

    Post formulario

---
+++ BlazorAppCodigo/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorAppCodigo.Models
+{
+    public class ProductData
+    {
+        public int year { get; set; }
+        public double price { get; set; }
+        public string CPU_model { get; set; }
+        public string Hard_disk_size { get; set; }
+    }
+
+    public class Product
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public ProductData data { get; set; } = new ProductData();
+    }
+}
