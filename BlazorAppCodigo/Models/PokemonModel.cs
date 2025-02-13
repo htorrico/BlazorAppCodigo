b0e1e59cf122d5c0ab04907cb31568a672ab3e08
Hugo Torrico <torricomarquezh@gmail.com>
2025-02-12 21:30:05 -0500

    Agregar archivos de proyecto.

---
+++ BlazorAppCodigo/Models/PokemonModel.cs
@@ -0,0 +1,19 @@
+﻿namespace BlazorAppCodigo.Models
+{
+    public class PokemonModel
+    {
+        public int pokemon_id { get; set; }
+        public string form { get; set; }       
+        public string pokemon_name { get; set; }
+     
+        public List<Evolution> evolutions { get; set; }
+     
+    }
+    public class Evolution
+    {
+        public int candy_required { get; set; }
+        public string form { get; set; }
+        public int pokemon_id { get; set; }
+        public string pokemon_name { get; set; }
+    }
+}
